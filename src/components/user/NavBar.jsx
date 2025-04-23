9f08a4f22629277597b9c9f15a4bec659c2fb75b
[200~Rameeskc <rameeskc72@gmail.com>
2025-04-24 01:50:27 +0530

    nav bar edited

--- src/components/user/NavBar.jsx
+++ src/components/user/NavBar.jsx
@@ -8,12 +8,12 @@ const NavBar = () => {
     <div className=' h-16 flex items-center justify-between lg:justify-end bg-[rgb(24,27,34)]'>
       <div className='flex '>
         <FaHeadphones className='text-orange-500 text-2xl lg:hidden ml-5' />
-        <p className=' font-Pacifico ml-2 text-xl text-slate-200'>MoonBeats</p>
+        <p className='lg:hidden font-Pacifico ml-2 text-xl text-slate-200'>MoonBeats</p>
       </div>
       <input
         type="text"
         placeholder='Search your song ....'
-        className='hidden lg:block border-slate-200 border-2 h-10 w-72 placeholder:text-sm rounded-4xl focus:outline-none text-sm text-[rgb(176,176,176)] p-4  mr-16' />
+        className='hidden lg:block border-slate-200 border-2 h-10 w-72 placeholder:text-sm rounded-full focus:outline-none text-sm text-[rgb(176,176,176)] p-4  mr-16' />
       <FaUserCircle className=' text-2xl text-slate-200 mr-5' />
     </div>
 
