6e78e3785afba28c2dec5e743a69bd8dc6e9cb54
[200~Rameeskc <rameeskc72@gmail.com>
2025-06-18 19:29:55 +0530

    banner area added

---
+++ src/components/user/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className=' h-96 bg-white'>Banner</div>
+  )
+}
+
+export default Banner
--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Banner from '../../components/user/Banner';
 
 const Home = () => {
   return (
+    <>
+      <Banner/>
     <div className="text-white p-6">
       <h1 className="text-3xl font-bold">Welcome to MoonBeats 🎧</h1>
       <p className="mt-2 text-slate-300">Your music world begins here.</p>
     </div>
+    </>
   );
 };
 
